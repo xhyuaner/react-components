a25e6102ef503a7cbeab565334fe90b72b4961f6
xhyuaner <xhyuaner@gmail.com>
2024-02-25 12:15:42 +0800

    feat: add redux store

--- 4-blog-react/src/index.js
+++ 4-blog-react/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import {RouterProvider} from 'react-router-dom'
 import router from './router'
+import { Provider } from'react-redux'
+import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+      <Provider store={store}>
+        <RouterProvider router={router}/>
+      </Provider>
   </React.StrictMode>
 );
 
