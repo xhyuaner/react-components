d4d007c85add5cd2f73deb8c0612cac3397d5ee5
xhyuaner <xhyuaner@gmail.com>
2024-02-26 15:05:49 +0800

    perf: lazy loading of routing

--- 4-blog-react/src/router/index.js
+++ 4-blog-react/src/router/index.js
@@ -1,8 +1,16 @@
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
-
+// import Home from '@/pages/Home'
+// import Article from '@/pages/Article'
+// import Publish from '@/pages/Publish'
+import { lazy,Suspense } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import {AuthRoute} from "@/components/AuthRoute"
+
+// lazy函数对组件进行导入
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publish = lazy(() => import('@/pages/Publish'))
 
 const router = createBrowserRouter([
     {
