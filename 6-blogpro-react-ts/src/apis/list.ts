c083abb88a5753b403d184d252883e66a5368b98
xhyuaner <xhyuaner@gmail.com>
2024-02-27 15:19:23 +0800

    feat: complete home page

--- 6-blogpro-react-ts/src/apis/list.ts
+++ 6-blogpro-react-ts/src/apis/list.ts
@@ -1,21 +1,54 @@
 import { http } from '@/utils'
 
-import type { ResType } from '@/apis/shared'
+import type { ResType } from './shared'
 
-// 2. 定义具体接口类型
-type ChannelItem = {
-    id: number
-    name: string
+//  2. 定义具体的接口类型
+
+export type ChannelItem = {
+  id: number
+  name: string
 }
 
 type ChannelRes = {
-    channels: ChannelItem[]
+  channels: ChannelItem[]
 }
 
-// 发送请求
+// 请求频道列表
+
 export function fetchChannelAPI() {
-    return http.request<ResType<ChannelRes>>({
-        url: '/channels',
-        method: 'get'
-    })
+  return http.request<ResType<ChannelRes>>({
+    url: '/channels',
+  })
+}
+
+// 请求文章列表
+type ListItem = {
+  art_id: string
+  title: string
+  aut_id: string
+  comm_count: number
+  pubdate: string
+  aut_name: string
+  is_top: number
+  cover: {
+    type: number
+    images: string[]
+  }
+}
+
+export type ListRes = {
+  results: ListItem[]
+  pre_timestamp: string
+}
+
+type ReqParams = {
+  channel_id: string
+  timestamp: string
+}
+
+export function fetchListAPI(params: ReqParams) {
+  return http.request<ResType<ListRes>>({
+    url: '/articles',
+    params,
+  })
 }
