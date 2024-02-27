8645de597a21d79c0d430af6b88d3f0289842829
xhyuaner <xhyuaner@gmail.com>
2024-02-27 14:07:17 +0800

    alias config

--- 6-blogpro-react-ts/src/main.tsx
+++ 6-blogpro-react-ts/src/main.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router/index.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <RouterProvider router={router} />
+)
